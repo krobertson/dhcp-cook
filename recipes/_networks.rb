8f8d94533f6dcdabd2efa9e62110de591fcacb10
Ken Robertson <ken@invalidlogic.com>
2013-06-03 16:59:55 -0700

    Remove the use of databags and fix a bug with settings routers in the subnet template

--- recipes/_networks.rb
+++ recipes/_networks.rb
@@ -2,14 +2,7 @@
 #
 # Setup subnets
 #
-if node[:dhcp][:networks].empty?
-  raise Chef::Exceptions::AttributeNotFound, "node[:dhcp][:networks] must contain entries for dhcpd to operate"
-end
-
-node[:dhcp][:networks].each do |net|
-  net_bag = data_bag_item( node[:dhcp][:networks_bag], Helpers::DataBags.escape_bagname(net) )
-
-  next unless net_bag
+node[:dhcp][:networks].each do |net_bag|
   # run the lwrp with the bag data
   dhcp_subnet net_bag["address"] do
     broadcast net_bag["broadcast"]
